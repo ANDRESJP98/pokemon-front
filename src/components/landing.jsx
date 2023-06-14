6e3cc6d2abc08c416d789dd9d17b73b2539bec31
Andres Jose Paternina Rubiano <apaterninar@gmail.com>
2023-06-13 20:42:20 -0500

    a

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -4,7 +4,7 @@ import style from './landing.module.css'
 export default function Landing(){
     return (
         <div className={style.image}>
-            <h1 className={style.text}>START</h1>
+            <h1 className={style.text}>STAqqRT</h1>
             <Link to='/home'> 
             <button className={style.button}></button>
             </Link>
